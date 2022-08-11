20ea4d8737fefc5581f9b68111c7852cc7f5eae6
Cjorasia <jorasiac@gmail.com>
2022-08-11 16:30:26 +0530

    qrcode added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+// import "react-loader-spinner/dist/loader/css/react-loader-spinner.css";
+
+// Rinkeby chainId
+const supportedChainIds = [4];
+const connectors = {
+ injected: {},
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+ return (
+  // Wrapping ThirdWeb Login... can access authentication in any of the pages or component
+  <ThirdwebWeb3Provider
+   supportedChainIds={supportedChainIds}
+   connectors={connectors}
+  >
+   <Component {...pageProps} />
+  </ThirdwebWeb3Provider>
+ );
 }
 
-export default MyApp
+export default MyApp;
